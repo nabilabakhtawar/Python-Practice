8baeb81ae7e7f8cbdf68654dd7a1675dc1be4850
Nabila Bakhtawar <nabilabakhtawar@gmail.com>
2026-09-20 22:26:17 +0500

    Variables And Datatypes in python

---
+++ day1.py/Fristprogram.py
@@ -0,0 +1,35 @@
+print("Hello World")
+print("This is my frist program in python")
+
+print("Nabila Bakhtawar is my name ")
+print("my age is 21 ")
+
+print("Nabila Bakhtawar is my name","my age is 21 ")
+
+print(23)
+print(23+23)
+
+name="Nabila Bakhtawar"
+age=21
+price=23.5
+
+print(name)
+print(age)
+print(price)
+
+print("name")
+print("age")
+print("price")
+
+print("my name is ",name,"and my age is ",age,"and the price is ",price)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+age=23
+old=False
+a=None
+print(type(age))
+print(type(old))    
+print(type(a))
