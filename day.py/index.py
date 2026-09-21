8393284f34eb3f902826af06f41451e2c365a561
Nabila Bakhtawar <nabilabakhtawar@gmail.com>
2026-09-21 23:27:45 +0500

    Basics of python

---
+++ day.py/index.py
@@ -0,0 +1,60 @@
+
+#sum of two numbers
+a=2
+b=3
+sum=a+b
+print(sum)
+
+# a=2
+# b=3
+# diff=a-b
+# print(diff)
+
+#comments in python 
+print("this is a comment in python")
+
+#arithmetic operators
+# a=10
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) #used to find the remainder (modular function)
+# print(a**b)
+ 
+ 
+#relational operators
+# a=50
+# b=20
+# print(a==b)#False
+# print(a!=b)#True
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+#Assignmnet operator
+# num=10
+# num+=5
+# print(num)
+# num +=10
+# print(num)
+# num -=5
+# print(num)
+# num *=2
+# print(num)
+# num /=5
+# print(num) 
+
+#Logical operators
+a=100
+b=50
+print(not (a>b))
+print(not False)
+val1=True
+val2=True
+print("and operator is:",val1 and val2)
+val1=True
+val2=False
+print("OR operator is:",val1 or val2)
