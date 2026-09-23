55f3b1e1c9db6eeed8d1345e0eef2e303bdf0096
Nabila Bakhtawar <nabilabakhtawar@gmail.com>
2026-09-23 20:07:07 +0500

    practice code

---
+++ day3.py/index.py
@@ -0,0 +1,48 @@
+#type conversion
+a=12
+b=15.7
+sum=a+b
+print(sum)
+
+#type casting
+a=int("5")
+b=4.5
+print(type(a))
+sum=(a+b)
+print(sum)
+
+a=float("5")
+b=4.5
+print(type(a))
+sum=(a+b)
+print(sum)
+
+# #input in python
+# name=input ("enter your name:")
+# print("welcome",name)
+
+# val= int(input("enter your value:"))
+# print(type(val),val)
+
+# name=input("enter your name:")
+# age=input("enter your age:")
+# marks=input("enter your marks:")
+
+#  print("welcome",name)
+#  print("age:",age)
+ #print("marks:",marks)30
+
+#program to add two numbers
+num1 = int(input("enter your frist number:"))
+num2 = int(input("enter your second number:"))
+print("sum = ", num1+num2)
+
+#program to find the area of sqaure
+
+side=float(input("enter the side of square:"))
+print("area=",side*side)
+
+#progrm to find average of two numbers
+a= float(input("enter frist number :"))
+b= float(input("enter second number:"))
+print("avg:",(a+b)/2)
